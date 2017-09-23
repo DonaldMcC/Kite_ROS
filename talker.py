ea941df1e51c7890556977c5d2f4ae9cfd9cc786
DonaldMcC <donaldm2020@gmail.com>
2017-09-23 21:47:00 +0100

    start on custom message and about to move git root

--- talker.py
+++ talker.py
@@ -2,7 +2,7 @@
 # from ros wiki for initial testing
 import rospy
 from std_msgs.msg import String
-
+from custommsg.msg import Kitepos
 
 def talker():
     pub = rospy.Publisher('chatter', String, queue_size=10)
@@ -16,14 +16,22 @@ def talker():
     return
 
 
-def kite_pos(position, routepoints, priorpos):
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('kitepos', anonymous=True)
+def kite_pos(posx, posy, kiteangle, dirx, diry, routepoints, priorpos):
+    pub = rospy.Publisher('custom_chatter', Kitepos, queue_size=10)
+    rospy.init_node('custom_talker', anonymous=True)
     rate = rospy.Rate(10)  # 10hz
+    msg = Kitepos()
+    msg.name = "Kite Position"
+    msg.posx = posx
+    msg.posy = posy
+    msg.kiteangle = kiteangle
+    msg.dirx = dirx
+    msg.diry = diry
+
     while not rospy.is_shutdown():
         hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
+        rospy.loginfo(msg)
+        pub.publish(msg)
         rate.sleep()
     return
 
