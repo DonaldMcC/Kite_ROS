e01f72d4a411115f8703a6bd507142bdde6bab8a
donald <donaldm2020@gmail.com>
2017-12-31 19:10:49 +0000

    more kiteb_process

--- scripts/kiteb_process.py
+++ scripts/kiteb_process.py
@@ -13,13 +13,34 @@ def get_force(messageval, minohm, maxohm):
     force = 0
     return force
 
-def get_angle(rcent):
+
+def linearmap(value, minx, maxx, miny, maxy):
+    return miny + (value-minx)/(maxx-minx) * (maxy-miny)
+
+
+def get_angle(rcent, centremaxleft, centremiddle, centremaxright, maxangleleft, maxangleright):
     # need to establish if resistor is linear across range - probalby some graphing required and also what
     # are the max angle then we have a plot to work with
-    a=params['centremaxleft']
-    b=params['centremiddle']
-    c=params['centremaxright']
-    angle = 0
+    # TODO will need maxangle for this as well
+
+
+    # allow resistor to operate either way +ve
+    if centremaxleft < centremaxright:
+        if rcent >= centremiddle and rcent<= centremaxright:
+            angle = linearmap(rcent, centremiddle, centremaxright, 0, maxangleright)
+        elif rcent >= centremaxleft and rcent < centremiddle:
+            #this should return a -ve and maxangleleft should also be set as -ve
+            angle = linearmap(rcent, centremiddle, centremaxleft, 0, maxangleleft)
+        else:
+            print('centre angle outwith permitted range')
+    else:  # centremaxleft is +ve so we need to invert mapping
+        if rcent <= centremiddle and rcent >= centremaxright:
+            angle = linearmap(rcent, centremiddle, centremaxright, 0, maxangleright*-1)
+        elif rcent <= centremaxleft and rcent > centremiddle:
+            # this should return a -ve and maxangleleft should also be set as -ve
+            angle = linearmap(rcent, centremiddle, centremaxleft, 0, maxangleleft * -1)
+        else:
+            print('centre angle outwith permitted range')
 
     return angle
 
@@ -29,5 +50,5 @@ def proc_arduino(message):
     answer = {}
     answer['forceleft'] = get_force(message.rleft, params.leftmin, params.leftmax)
     answer['forceright'] = get_force(message.rright, params.rightmin, params.rightmax)
-    answer['barangle'] = get_angle(message.rcent)
+    answer['barangle'] = get_angle(message.rcent, params.centremaxleft, params.centremiddle, params.centremaxright)
     return answer
