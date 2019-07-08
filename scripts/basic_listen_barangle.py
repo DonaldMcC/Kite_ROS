cba2367bd51978236af7ab1793199ccdc515f9b6
donald <donaldm2020@gmail.com>
2019-07-08 12:30:23 +0100

    listen_barangle now working with kiteangle message

--- scripts/basic_listen_barangle.py
+++ scripts/basic_listen_barangle.py
@@ -1,28 +1,32 @@
 #!/usr/bin/env python
 # from ros wiki for initial testing
+
 import rospy
-from kite_ros.msg import Kitebase
-barangle = 6
+from std_msgs.msg import Int16
+barangle = 0
+
 
 def callback(data):
     global barangle
-    barangle = data.barangle
-    #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.barangle)
+    barangle = data.data
+    print(barangle)
     return
 
 
-def listen_kitebase():
+def listen_kiteangle():
     # In ROS, nodes are uniquely named. If two nodes with the same-
     # node are launched, the previous one is kicked off. The
     # anonymous=True flag means that rospy will choose a unique
     # name for our 'listener' node so that multiple listeners can
     # run simultaneously.
-    #rospy.init_node('kitebar_listen', anonymous=True)
-    #pub = rospy.Publisher('kitebase_node', Kitebase, queue_size=10)
-    rospy.Subscriber('kitebase_node', Kitebase, callback)
+    rospy.init_node('kiteangle_listen', anonymous=True)
+    # pub = rospy.Publisher('kitebase_node', Kitebase, queue_size=10)
+    # rospy.Subscriber('kitebase_node', Kitebase, callback)
+    rospy.Subscriber('kiteangle', Int16, callback)
 
-    #spin() simply keeps python from exiting until this node is stopped
-    #rospy.spin()
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
 
 def get_barangle(kite, base, control):
     global barangle
@@ -30,11 +34,11 @@ def get_barangle(kite, base, control):
         if base.updatemode < 2:
             return base.barangle
         else:  # when kiteangle is driving the barangle
-            #TO DO add kitebar ration to this
+            # TO DO add kitebar ratio to this
             return kite.kiteangle
-    else: # automated flight reading from some sort of sensor via ROS
+    else:  # automated flight reading from some sort of sensor via ROS
         return barangle
 
 
 if __name__ == '__main__':
-    listen_kitebase()
+    listen_kiteangle()
