97153388ea3e9b757cf4364d443ab2386224ba02
Donald McClymont <donaldm2020@gmail.com>
2021-04-10 12:47:26 -0400

    ongoing log build

--- scripts/logging.py
+++ scripts/logging.py
@@ -1,19 +1,20 @@
+import csv
 from cvwriter import initwriter, writeframe
 
-def writelogheader():
+def writelogheader(config, kite, base, control, height, width, fps):
     # THis should initialise the logging
-    pass
-
-def writelogs(kite, base, control, config, height, width, fps):
     if config.logging and config.writer is None:
         # h, w = frame.shape[:2]
         # height, width = 480, 640 - removed should now be set above
         # height, width, channels = frame.shape
         config.writer = initwriter("record.avi", height, width, fps)
+        config.csvwriter
         origwriter = initwriter("origrecord.avi", height, width, fps)
 
+
+def writelogs(config, kite, base, control, frame, height, width, fps):
     if config.logging:  # not saving this either as it errors on other screen
-        writeframe(writer, frame, height, width)
+        writeframe(config.writer, frame, height, width)
 
 def close():
     pass
